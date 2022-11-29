9620ed979396fe2dbd60c11b73231ade54a3717a
Dreamer <dreamer13sq@gmail.com>
2022-11-29 03:27:25 -0500

    Hotmenu Action Property Updates Scene Range
    When changing the action via the Hot Menu, the scene frame range updates accordingly. (3.2.2 only)

--- DmrBlender_Tools/dmr_hotmenu.py
+++ DmrBlender_Tools/dmr_hotmenu.py
@@ -42,6 +42,7 @@ class DMR_PT_HotMenu(bpy.types.Panel): # ------------------------------
                     if m:
                         r.prop(m, 'use_clip', text="", icon='NODE_SIDE')
         
+        # Toggles
         row = layout.row(align = 1)
         row.scale_x = 4.0
         row.scale_y = 1.0
@@ -54,18 +55,20 @@ class DMR_PT_HotMenu(bpy.types.Panel): # ------------------------------
         
         row.prop(context.tool_settings, "use_keyframe_insert_auto", text="", toggle=True)
         
+        # Action
         if obj:
             if obj.mode == 'EDIT':
                 layout.prop(bpy.context.scene.tool_settings, 'proportional_size')
             
             if obj.animation_data:
                 #layout.template_ID(obj.animation_data, "action", new="action.new", unlink="action.unlink")
-                layout.prop(obj.animation_data, 'action')
+                r = layout.row()
+                r.prop(obj, 'active_action')
+                r.prop(context.scene, 'sync_action_frame_range', text='', toggle=True, icon='FILE_REFRESH')
             if obj.find_armature() and obj.find_armature().animation_data:
                 r = layout.row()
-                rr = r.row()
-                rr.scale_x = 0.4
-                layout.prop(obj.find_armature().animation_data, 'action', text='Parent')
+                r.prop(obj.find_armature(), 'active_action', text='Parent')
+                r.prop(context.scene, 'sync_action_frame_range', text='', toggle=True, icon='FILE_REFRESH')
         
         row = layout.row(align=1)
         row.scale_x = 2.0
@@ -75,9 +78,25 @@ classlist.append(DMR_PT_HotMenu)
 
 # =============================================================================
 
+def ActionChange(self, context):
+    action = self.active_action
+    self.animation_data.action = action
+    
+    if context.scene.sync_action_frame_range and action.use_frame_range:
+        context.scene.frame_start = int(action.frame_start)
+        context.scene.frame_end = int(action.frame_end)
+
 def register():
     for c in classlist:
         bpy.utils.register_class(c)
+    
+    bpy.types.Object.active_action = bpy.props.PointerProperty(
+        name="Action", type=bpy.types.Action, update=ActionChange
+    )
+    
+    bpy.types.Scene.sync_action_frame_range = bpy.props.BoolProperty(
+        name="Sync Action Frame Range", default=True
+    )
 
 def unregister():
     for c in reversed(classlist):
